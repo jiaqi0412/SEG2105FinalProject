6873ef3417345bdd3af8924b68a80224a93e8586
Rageesan Pushparajah <rpush078@uottawa.ca>
2018-11-18 16:31:03 -0500

    New updates

--- app/src/main/java/com/example/lijia/finalproject/serviceProviderProfileInfoPage.java
+++ app/src/main/java/com/example/lijia/finalproject/serviceProviderProfileInfoPage.java
@@ -1,54 +1,23 @@
 package com.example.lijia.finalproject;
 
-import android.app.Application;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.provider.MediaStore;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import com.squareup.picasso.Picasso;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class serviceProviderProfileInfoPage extends AppCompatActivity {
     private TextView tvAddress, tvPhone, tvCompanyName, tvGeneralDescription, tvLicensed;
@@ -60,18 +29,16 @@ public class serviceProviderProfileInfoPage extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
 
     private Button updateButton;
-    private Button loadButton;
     private FirebaseDatabase firebaseDatabase;
     RadioGroup licensedRadioGroup;
     RadioButton yesOrNoOption;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_provider_profile_info_page);
 
+        Button updateButton = (Button) findViewById(R.id.updateButton);
         tvAddress = findViewById(R.id.tvAddress);
         tvPhone = findViewById(R.id.tvPhone);
         tvCompanyName = findViewById(R.id.tvCompanyName);
@@ -124,24 +91,17 @@ public class serviceProviderProfileInfoPage extends AppCompatActivity {
 
 
 
-
-
-
         updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(validate()){
 
                     //sendEmailVerification();
-                                sendUserData();
-                                firebaseAuth.signOut();
-                                Toast.makeText(serviceProviderProfileInfoPage.this,
-                                        "Successfully Registered, Upload complete!", Toast.LENGTH_SHORT).show();
-                                //finish();
-                                startActivity(new Intent(serviceProviderProfileInfoPage.this, ServiceProviderProfile.class));
-
-
-
+                    sendUserData();
+                    //firebaseAuth.signOut();
+                    Toast.makeText(serviceProviderProfileInfoPage.this, "Successfully Registered, Upload complete!", Toast.LENGTH_SHORT).show();
+                    //finish();
+                    startActivity(new Intent(serviceProviderProfileInfoPage.this, ServiceProviderProfile.class));
 
                 }
             }
@@ -180,7 +140,6 @@ public class serviceProviderProfileInfoPage extends AppCompatActivity {
     }
 
     private void sendUserData(){
-
         DatabaseReference myRef = firebaseDatabase.getReference(firebaseAuth.getUid());
         ServiceProviderProfile userProfile = new ServiceProviderProfile(address,
                 phone, companyName, generalDescription, strYesOrNo);
@@ -194,7 +153,6 @@ public class serviceProviderProfileInfoPage extends AppCompatActivity {
         startActivity(i);
 
     }
-
 
 
 }
